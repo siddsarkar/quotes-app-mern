eb71adfd9cdfccbb74a900425f980fbcf3ee02a3
Siddhartha Sarkar <siddsarkar7@gmail.com>
2020-10-22 00:50:22 +0530

    server side logic on likes

--- client/src/store/actions/likesActions.js
+++ client/src/store/actions/likesActions.js
@@ -26,10 +26,19 @@ export const getLikesForArticle = (articleId, cb) => {
   return (dispatch) => {
     fetch("/api/likes/get/" + articleId, {
       method: "GET",
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem("jwtToken"),
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
     })
       .then((res) => res.json())
       .then((res) => {
-        dispatch({ type: actionTypes.GOT_LIKES_FOR_ARTICLE, likes: res });
+        dispatch({
+          type: actionTypes.GOT_LIKES_FOR_ARTICLE,
+          likes: res.likes,
+          isLiked: res.isLiked,
+        });
         cb();
       });
   };
